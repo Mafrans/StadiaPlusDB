b7673f5c08f4c9b8c4f04e7e0ccf859b8c20ee61
Mafrans <malte.kluft@gmail.com>
2020-08-14 01:23:02 +0200

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,83 @@
 const path = require('path');
+const TypedocWebpackPlugin = require('typedoc-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+module.exports = env => {
+    return {
+        entry: {
+            bundle: './src/index.js',
+        },
+        devtool: 'inline-source-map',
+        mode: env.production ? 'production' : 'development',
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+                {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+                },
+                {
+                    test: /\.css$/i,
+                    use: [
+                        // Creates `style` nodes from JS strings
+                        {
+                            loader: 'style-loader',
+                            options: {
+                                insert: 'html',
+                            }
+                        },
+                        'css-loader'
+                    ],
+                },
+                
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        // Creates `style` nodes from JS strings
+                        {
+                            loader: 'style-loader',
+                            options: {
+                                insert: 'html',
+                            }
+                        },
+                        // Translates CSS into CommonJS
+                        'css-loader',
+                        // Compiles Sass to CSS
+                        'sass-loader',
+                    ],
+                },
+                {
+                    test: /\.(png|jpe?g|gif)$/i,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                        },
+                    ],
+                },
+                {
+                    test: /\.txt$/i,
+                    use: 'raw-loader',
+                },
+            ],
+        },
+        plugins: [
+            new VueLoaderPlugin(),
+            new TypedocWebpackPlugin({
+                name: 'Contoso',
+                mode: 'file',
+                out: './docs',
+            }, './src')
+        ],
+        resolve: {
+            extensions: [ '.tsx', '.ts', '.js' ],
+        },
+        output: {
+            filename: '[name].js',
+            path: path.resolve(__dirname, 'dist'),
+        },
     }
 };
