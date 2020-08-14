a6fd720859aa8e69ed8b8d98d3cf8039bbda24db
Mafrans <malte.kluft@gmail.com>
2020-08-14 02:51:27 +0200

    Game data interfaces

---
+++ src/database/IGameData.ts
@@ -0,0 +1,25 @@
+export interface IGameData {
+    game: IGame;
+    achievements: IAchievement[];
+    user: IUser;
+    time: number;
+}
+
+export interface IAchievement {
+    name: string;
+    description: string;
+    value: number;
+    icon: string;
+    game: string;
+}
+
+interface IGame {
+    uuid: string;
+    name: string;
+}
+
+interface IUser {
+    name: string;
+    tag: string;
+    avatar: string;
+}
