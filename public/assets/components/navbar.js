15a0704eecc08f07ed91f6d60abff041b2d8e236
Mafrans <malte.kluft@gmail.com>
2020-08-19 22:48:11 +0200

    Include the rest of the info in the navbar

--- public/assets/components/navbar.js
+++ public/assets/components/navbar.js
@@ -1,18 +1,46 @@
+import Button from '/components/button.js';
+
 var component = {
     template: `
         <div class="navbar uk-width-1-1">
             <div class="uk-container uk-container-large" style="height: 100%">
-                <div class="nav-logo-container uk-flex uk-flex-middle uk-height-1-1">
-                    <img class="nav-logo uk-margin-right" src="/images/logo.svg">
-                    <h1 class="nav-logo-title uk-text-bold c-gradient">STADIA+</h1>
-                    <h1 v-if="showDb" class="nav-logo-db uk-text-bolder uk-margin-small-left c-cherry">DB</h1>
+                <div class="uk-height-1-1" uk-grid>
+                    <div @click="goToHome()" class="nav-logo-container uk-flex uk-flex-middle uk-height-1-1 uk-width-1-2">
+                        <img class="nav-logo uk-margin-right" src="/images/logo.svg">
+                        <h1 class="nav-logo-title uk-text-bold c-gradient" style="letter-spacing: 2px">STADIA+</h1>
+                        <h1 v-if="showdb != undefined" class="nav-logo-db uk-text-bolder uk-margin-small-left c-cherry">DB</h1>
+                    </div>
+                    <div class="uk-width-1-2 uk-flex uk-flex-right uk-flex-middle">
+                        <a @click="goToHome()" class="c-dark-gray uk-text-medium uk-margin-medium-right">Home</a>
+                        <a @click="openWikiPage()" class="c-dark-gray uk-text-medium uk-margin-medium-right">Help & docs</a>
+                        <btn class="uk-margin-small-left" icon="get_app" @click="openExtensionPage()" gradient=""> Get the free extension </btn>
+                    </div>
                 </div>
-                <slot></slot>
             </div>
         </div>
     `,
 
-    props: [ 'show-db' ]
+    props: [ 'showdb' ],
+    methods: {
+        openExtensionPage() {
+            window.open(
+                'https://chrome.google.com/webstore/detail/stadia%20-extension/bbhmnnecicphphjamhdefpagipoegijd',
+                '_blank'
+            );
+        },
+        openWikiPage() {
+            window.open(
+                'https://github.com/Mafrans/StadiaPlus/wiki',
+                '_blank'
+            );
+        },
+        goToHome() {
+            location.href = location.origin;
+        }
+    },
+    components: {
+        'btn': Button
+    }
 };
 
 export default component;
