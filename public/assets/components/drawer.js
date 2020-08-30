0f1be39a75b96185c64555100eeb90fc95986b91
Mafrans <malte.kluft@gmail.com>
2020-08-30 17:15:19 +0200

    Switch to the yarn version of uikit and add images

--- public/assets/components/drawer.js
+++ public/assets/components/drawer.js
@@ -1,8 +1,12 @@
 var component = {
     template: `
-        <div id="drawer" uk-offcanvas="overlay: true, mode: push">
+        <div id="drawer" uk-offcanvas="overlay: false, mode: push">
             <div class="uk-offcanvas-bar">
-                <h1>Test</h1>
+                <div class="uk-flex uk-flex-middle">
+                    <img class="nav-logo uk-margin-right" src="/images/logo.svg">
+                    <h1 class="nav-logo-title uk-text-bold c-gradient" style="letter-spacing: 2px">STADIA+</h1>
+                </div>
+                <hr>
                 <p>Testest</p>
                 <button class="uk-offcanvas-close" type="button" uk-close></button>
             </div>
