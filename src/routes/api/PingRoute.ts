496000bdf50750cd3aa311ca26d616a0e330f3be
Mafrans <malte.kluft@gmail.com>
2021-01-26 12:39:27 +0100

    Update ping route to include simple authentication ping.

--- src/routes/api/PingRoute.ts
+++ src/routes/api/PingRoute.ts
@@ -1,8 +1,18 @@
 import { RouteInterface } from "../Route.interface";
+import { Request, Response } from 'express';
+import { Google } from "../../models/Google.model";
 
 
 export class PingRoute implements RouteInterface {
-    get(req: any, res: any, next: any) {
-        res.send('connected!');
+    async get(req: Request, res: Response, next: any) {
+        let authenticated = false;
+        if (req.query.hasOwnProperty('token') && req.query.token !== undefined && req.query.token !== null) {
+            const gaia = await Google.GET_ID(req.query.token.toString());
+    
+            if (gaia !== null) {
+                authenticated = true;
+            }
+        }
+        res.send({ connected: true, authenticated });
     }
 }
