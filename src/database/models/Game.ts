7f778bccf93780c57b6a522cdf4ff9aed42be16a
Mafrans <malte.kluft@gmail.com>
2021-04-05 00:42:27 +0200

    feat: add Game.ts model

---
+++ src/database/models/Game.ts
@@ -0,0 +1,13 @@
+import { Document, model, Schema } from "mongoose"
+
+const GameSchema = new Schema({
+    id: { type: String, required: true },
+    name: { type: String, required: true }
+})
+
+export interface Game extends Document {
+    id: string
+    name: string
+}
+
+export default model<Game>('Game', GameSchema);
