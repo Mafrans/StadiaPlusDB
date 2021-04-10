b8d4a6a34d73f3e5e27a1fac195561ee4e24e99a
Mafrans <malte.kluft@gmail.com>
2021-04-10 13:47:41 +0200

    feat: add boilerplate frontend

---
+++ frontend/src/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Router from "./Router";
+
+const mountNode = document.getElementById("app");
+ReactDOM.render(<Router />, mountNode);
